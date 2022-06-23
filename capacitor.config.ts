4a878fb10512d0cc2b394d95af7a83318e90a2e9
Seun Odeyemi <seunodeyemi@Clevers-MacBook-Pro.local>
2022-06-23 19:11:42 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plan-a-frontend-challenge-oluwaseun-odeyemi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
